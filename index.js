fd965a5295e9e445d2d1044cb24732f4749ca7bc
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-03-02 19:32:59 -0500

    added new routes and body parser

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const mysql = require("mysql");
 const pool = require("./sql/connection");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome to the eden application");
 });
 
-// app.get("/user", (req, res) => {
-//   pool.query("select * from User_credentials", (rows) => {
-//     // if (err) throw new error(err);
-//     return res.json(rows);
-//   });
-// });
 app.get("/user", (req, res) => {
-  pool.query("select * from User_credentials", (err, rows) => {
+  pool.query(`SELECT * FROM User_credentials`, (err, rows) => {
     // if (err) throw new Error(err);
     return res.json(rows);
+  });
+});
+
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+
+  pool.query(`SELECT * FROM User_credentials WHERE id = ${id}`, (err, rows) => {
+    return res.json(rows);
+  });
+
+  app.post("/user", (req, res) => {
+    const User_credentials = req.body;
+
+    pool.query(
+      `INSERT INTO User_credentials(User_Name, User_Password, User_Email) VALUES (${User_credentials.User_Name},${User_credentials.User_Password}, ${User_credentials.User_Email})`,
+      (err, rows) => {
+        return req.body;
+      }
+    );
   });
 });
 app.listen(PORT, console.log("I am listening on port 5000"));
