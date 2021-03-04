35bbbbcbd323b2cfcc3a8e7e87c9ae6e484e0eeb
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-03-03 19:12:52 -0500

    added more routes

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ app.get("/", (req, res) => {
 
 app.get("/user", (req, res) => {
   pool.query(`SELECT * FROM User_credentials`, (err, rows) => {
-    // if (err) throw new Error(err);
+    if (err) {
+      throw new Error(err);
+    }
     return res.json(rows);
   });
 });
@@ -23,19 +25,46 @@ app.get("/user/:id", (req, res) => {
   const id = req.params.id;
 
   pool.query(`SELECT * FROM User_credentials WHERE id = ${id}`, (err, rows) => {
+    if (err) {
+      throw new Error(err);
+    }
     return res.json(rows);
   });
-
-  app.post("/user", (req, res) => {
-    const User_credentials = req.body;
-
-    pool.query(
-      `INSERT INTO User_credentials(User_Name, User_Password, User_Email) VALUES (${User_credentials.User_Name},${User_credentials.User_Password}, ${User_credentials.User_Email})`,
-      (err, rows) => {
-        return req.body;
-      }
-    );
-  });
-  // You stopped at the 2:19 mark on video recording 1/14/2021
 });
-app.listen(PORT, console.log("I am listening on port 5000"));
+app.post("/user", (req, res) => {
+  const User_credentials = req.body;
+
+  pool.query(
+    `INSERT INTO User_credentials (User_Name, User_Password, User_Email) VALUES (${User_credentials.User_Name},${User_credentials.User_Password}, ${User_credentials.User_Email})`,
+    (err, rows) => {
+      if (err) {
+        throw new Error(err);
+      }
+      return res.json({ id: rows.insertId });
+    }
+  );
+});
+
+app.put("/user/:id", (req, res) => {
+  let sql = `UPDATE ?? SET ? WHERE ?? = ? `;
+  sql = mysql.format(sql, ["User_credentials", req.body, "id", req.params.id]);
+  pool.query(sql, (err, results) => {
+    if (err) {
+      throw new Error(err);
+    }
+    return res.json({ id: results.message });
+  });
+});
+
+app.delete("/user/:id", (req, res) => {
+  let sql = `DELETE FROM ?? WHERE ?? = ? `;
+  sql = mysql.format(sql, ["User_credentials", "id", req.params.id]);
+  pool.query(sql, (err, results) => {
+    if (err) {
+      throw new Error(err);
+    }
+    return res.json({ id: results.affectedRows });
+  });
+});
+
+app.listen(PORT, console.log(`I am listening on port ${5000}`));
