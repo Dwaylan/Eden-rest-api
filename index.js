82878e8fd9562b560b4619f4ca33f262cd40168e
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-04-11 19:01:41 -0500

    commented out things. Looking for connection bug

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const plants = require("./routers/plants");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(plants);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the eden application please submit user credentials");
+  res.send("Welcome to the eden application");
 });
 
 app.listen(PORT, console.log(`I am listening on port ${5000}`));
