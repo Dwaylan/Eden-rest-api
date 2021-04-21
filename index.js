d6f361b8c1ad82f42df492f5c2f7ed040e6c605a
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-04-21 14:37:42 -0400

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const plants = require("./routers/plants");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(plants);
 
