16d48df814c7c079469100075a064e4c1078e46a
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-05-04 07:24:39 -0400

    configuring header for CORS image rendering

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const pool = require("./sql/connection");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Content-Type", "application/json");
+  res.header("content-Type", "image/jpeg");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
 app.use(cors());
 app.use(bodyParser.json());
 app.use(plants);
