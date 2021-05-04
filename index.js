35b3b03344d55cf8c70b6bf45727ddb260dcdd65
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-05-04 07:41:43 -0400

    more content type heads

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Content-Type", "application/json");
-  res.header("Content-Type", "image/jpeg");
+  res.header("Content-Type", "image/jpeg, image/jpg, image/png");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, authorization"
