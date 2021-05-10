4d858ceda13f24022743c17c5cb956af2c44b9fc
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-05-10 19:26:38 -0400

    testing

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(plants);
-app.options("*", cors());
+// app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the eden application");
