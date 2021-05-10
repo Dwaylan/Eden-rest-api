7cffbb786c159b9953bcf09ac8241f89e551069c
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-05-10 19:07:49 -0400

    playing with CORS again

--- index.js
+++ index.js
@@ -7,38 +7,21 @@ const pool = require("./sql/connection");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Content-Type", "application/json");
-  res.header("Content-Type", "image/jpeg, image/jpg, image/png");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "*");
-  next();
-});
-app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Content-Type", "application/json");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, authorization"
+//   );
+//   res.header("Access-Control-Allow-Methods", "*");
+//   next();
+// });
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(plants);
-
-//The code below DID NOT WORK
-//  app.use(cors({
-//  origin: '*',
-//  methods: ['GET'],
-//  allowedHeaders: '*'
-//}))
-
-// The code below DID NOT WORK
-// app.use((req, res) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-//     return res.status(200).json({});
-//   }
-// });
+// app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the eden application");
