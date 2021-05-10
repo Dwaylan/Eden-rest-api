a84d234485d0099acf6baa3c72f0aecd04827ad6
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-05-10 19:18:19 -0400

    reconfigured my cors file with the chrome cors extension. It was the only was possible to get the app to bypassthe img block

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const pool = require("./sql/connection");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// The code below works but it may be a bit verbose
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Credentials", true);
@@ -18,10 +20,10 @@ const PORT = process.env.PORT || 5000;
 //   res.header("Access-Control-Allow-Methods", "*");
 //   next();
 // });
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(plants);
-// app.options("*", cors());
+app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the eden application");
